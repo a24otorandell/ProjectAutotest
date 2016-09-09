efbc39fc4a5aaf89d129ccdf4cad4d1e682e8f8b
vsolis272 <vsolis@angel24.es>
2016-09-09 09:29:53 +0200

    Actualización codigo   AT2TRFSU0010

---
+++ src/main/java/screen/AT2TRFSU0010/AT2TRFSU0010Data.java
@@ -0,0 +1,35 @@
+package screen.AT2TRFSU0010;
+
+import core.FileGestor.DataHarvester;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by vsolis on 09/09/2016.
+ */
+public class AT2TRFSU0010Data {
+
+    Map<String, String> data = new HashMap<>();
+    DataHarvester harvester;
+
+    public AT2TRFSU0010Data(String enviroment) {
+        this.harvester = new DataHarvester("C:/ProjectAutotest/src/main/resources AT2TRFSU0010Values.txt");
+        setData();
+    }
+
+    public Map<String, String> getData() {
+        return data;
+    }
+
+    public void setData() {
+        this.data = harvester.harvestAll();
+        if (data.get("empty").equals("true")) {
+            setDefaultData();
+        }
+    }
+
+    public void setDefaultData() {
+
+    }
+}
---
+++ src/main/java/screen/AT2TRFSU0010/AT2TRFSU0010Locators.java
@@ -0,0 +1,24 @@
+package screen.AT2TRFSU0010;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by vsolis on 09/09/2016.
+ */
+public class AT2TRFSU0010Locators {
+    Map<String, String> elements = new HashMap<>();
+
+    public AT2TRFSU0010Locators(String enviroment) {
+        setElements();
+    }
+
+    public Map<String, String> getElements() {
+        return elements;
+    }
+
+    public void setElements() {
+
+
+    }
+}
