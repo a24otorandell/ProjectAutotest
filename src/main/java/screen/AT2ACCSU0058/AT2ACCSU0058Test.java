92d5fef98f24de52cbddbb0dfc881827e921b7ab
jmrios <jmrios@angel24.es>
2016-10-19 11:32:30 +0200

    nueva pantalla mdmde0011 y toda su estructura. Eliminación de codigo comentado de ComoProcedures.

--- src/main/java/screen/AT2ACCSU0058/AT2ACCSU0058Test.java
+++ src/main/java/screen/AT2ACCSU0058/AT2ACCSU0058Test.java
@@ -14,38 +14,30 @@ public class AT2ACCSU0058Test {
 
     public AT2ACCSU0058Test() {
     }
-
     public AT2ACCSU0058Locators getLocators() {
         return locators;
     }
-
     public void setLocators(AT2ACCSU0058Locators locators) {
         this.locators = locators;
     }
-
     public AT2ACCSU0058Data getData() {
         return data;
     }
-
     public void setData(AT2ACCSU0058Data data) {
         this.data = data;
     }
-
     public void start(TestDriver driver) {
         setScreenInfo(driver);
         CommonProcedures.goToScreen(driver);
     }
-
     protected void setScreenInfo(TestDriver driver) {
         driver.getTestdetails().setMainmenu("Maintenance HSI mapping");
         driver.getTestdetails().setSubmenu("Accommodation");
         driver.getTestdetails().setScreen("Setup");
     }
-
     protected String getElements(String key) {
         return String.valueOf(this.locators.getElements().get(key));
     }
-
     protected String getData(String key) {
         return String.valueOf(this.data.getData().get(key));
     }
@@ -134,7 +126,6 @@ public class AT2ACCSU0058Test {
         }
         return true;
     }
-
     private boolean search_accommodation(TestDriver driver) {
         driver.getReport().addHeader("SEARCH RECORD", 3, false);
         Functions.break_time(driver, 30, 500);
@@ -203,7 +194,6 @@ public class AT2ACCSU0058Test {
         }
         return true;
     }
-
     private boolean interaction_edit_accommodation(TestDriver driver) {
         driver.getReport().addHeader("EDITION RECORD", 3, false);
         String where = " on EDITION";
@@ -282,7 +272,6 @@ public class AT2ACCSU0058Test {
         }
         return true;
     }
-
     private boolean qbe_accommodation(TestDriver driver) {
         driver.getReport().addHeader("QBE RECORD", 3, false);
         String where = " on QBE";
@@ -356,7 +345,6 @@ public class AT2ACCSU0058Test {
         }
         return true;
     }
-
     private boolean others_actions_accommodation(TestDriver driver) {
         driver.getReport().addHeader("OTHER ACTIONS - DETACH", 3, false);
         String where = " on DETACH";
@@ -368,7 +356,6 @@ public class AT2ACCSU0058Test {
         }
         return true;
     }
-
     private boolean delete_accommodation(TestDriver driver) {
         driver.getReport().addHeader("DELETE DATA", 3, false);
         String where = " on DELETE DATA";
