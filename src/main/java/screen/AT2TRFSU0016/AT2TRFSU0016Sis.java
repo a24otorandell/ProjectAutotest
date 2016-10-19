bfe025a7aeeff1dc3a7e81a5f1ad542f9c36e589
acarrillo2007 <acarrillo@angel24.es>
2016-10-19 15:05:44 +0200

    quitar espacios innecesarios de la trfsu0016

--- src/main/java/screen/AT2TRFSU0016/AT2TRFSU0016Sis.java
+++ src/main/java/screen/AT2TRFSU0016/AT2TRFSU0016Sis.java
@@ -16,39 +16,31 @@ public class AT2TRFSU0016Sis {
     public AT2TRFSU0016Sis() {
 
     }
-
     public AT2TRFSU0016Locators getLocators() {
         return locators;
     }
-
     public void setLocators(AT2TRFSU0016Locators locators) {
         this.locators = locators;
     }
-
     public AT2TRFSU0016Data getData() {
         return data;
     }
-
     public void setData(AT2TRFSU0016Data data) {
         this.data = data;
     }
-
     public void start(TestDriver driver) {
         setScreenInfo(driver);
         CommonProcedures.goToScreen(driver);
     }
-
     protected void setScreenInfo(TestDriver driver) {
         driver.getTestdetails().setMainmenu("Transfers");
         driver.getTestdetails().setSubmenu("Setup");
         driver.getTestdetails().setScreen("TTOO Quality Groups");
     }
-
     protected String getElements(String key) {
         String value = this.locators.getElements().get(key);
         return value;
     }
-
     protected String getData(String key) {
         String value = this.data.getData().get(key);
         return value;
--- src/main/java/screen/AT2TRFSU0016/AT2TRFSU0016Test.java
+++ src/main/java/screen/AT2TRFSU0016/AT2TRFSU0016Test.java
@@ -17,39 +17,31 @@ public class AT2TRFSU0016Test {
     public AT2TRFSU0016Test() {
 
     }
-
     public AT2TRFSU0016Locators getLocators() {
         return locators;
     }
-
     public void setLocators(AT2TRFSU0016Locators locators) {
         this.locators = locators;
     }
-
     public AT2TRFSU0016Data getData() {
         return data;
     }
-
     public void setData(AT2TRFSU0016Data data) {
         this.data = data;
     }
-
     public void start(TestDriver driver) {
         setScreenInfo(driver);
         CommonProcedures.goToScreen(driver);
     }
-
     protected void setScreenInfo(TestDriver driver) {
         driver.getTestdetails().setMainmenu("Transfers");
         driver.getTestdetails().setSubmenu("Setup");
         driver.getTestdetails().setScreen("TTOO Quality Groups");
     }
-
     protected String getElements(String key) {
         String value = this.locators.getElements().get(key);
         return value;
     }
-
     protected String getData(String key) {
         String value = this.data.getData().get(key);
         return value;
@@ -64,7 +56,6 @@ public class AT2TRFSU0016Test {
 
         return false;
     }
-
 
     private boolean Aditional_suplements(TestDriver driver) {
 
