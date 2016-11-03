0f0985448e94942a3fa14415a53437b9f2b41f32
acarrillo2007 <acarrillo@angel24.es>
2016-11-03 18:19:54 +0100

    completado nombre de la pantalla mdmcl0024

--- src/main/java/screen/AT2MDMCL0024/AT2MDMCL0024Sis.java
+++ src/main/java/screen/AT2MDMCL0024/AT2MDMCL0024Sis.java
@@ -57,6 +57,7 @@ public class AT2MDMCL0024Sis {
     private boolean interaction_record_commision(TestDriver driver) {
         driver.getReport().addHeader("CREATTION RECORD", 3, false);
         String where = " on CREATTION";
+        Functions.break_time(driver, 30, 500);
         if (!Functions.checkClick(driver,
                 new String[]{"commision_b_add", getElements("commision_b_add")}, //element to click
                 recursiveXPaths.glass, //element expected to appear
