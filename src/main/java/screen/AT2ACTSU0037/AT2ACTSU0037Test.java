f88c2f0e21892e5c7e29f7e61a98d5ea2c0addb4
acarrillo2007 <acarrillo@angel24.es>
2016-11-03 14:25:04 +0100

    quitar espacios innecesarios de la actsu0037Test

--- src/main/java/screen/AT2ACTSU0037/AT2ACTSU0037Test.java
+++ src/main/java/screen/AT2ACTSU0037/AT2ACTSU0037Test.java
@@ -16,38 +16,30 @@ public class AT2ACTSU0037Test {
 
     public AT2ACTSU0037Test() {
     }
-
     public AT2ACTSU0037Locators getLocators() {
         return locators;
     }
-
     public void setLocators(AT2ACTSU0037Locators locators) {
         this.locators = locators;
     }
-
     public AT2ACTSU0037Data getData() {
         return data;
     }
-
     public void setData(AT2ACTSU0037Data data) {
         this.data = data;
     }
-
     public void start(TestDriver driver) {
         setScreenInfo(driver);
         CommonProcedures.goToScreen(driver);
     }
-
     protected void setScreenInfo(TestDriver driver) {
         driver.getTestdetails().setMainmenu("Accommodations");
         driver.getTestdetails().setSubmenu("Setup");
         driver.getTestdetails().setScreen("Suppliers B2B 2.0");
     }
-
     protected String getElements(String key) {
         return String.valueOf(this.locators.getElements().get(key));
     }
-
     protected String getData(String key) {
         return String.valueOf(this.data.getData().get(key));
     }
@@ -61,6 +53,7 @@ public class AT2ACTSU0037Test {
         if (!delete_supplier(driver)) return false;
         return false;
     }
+
     private boolean interaction_record_supplier(TestDriver driver) {
         driver.getReport().addHeader("CREATTION RECORD", 3, false);
         String where = " on CREATTION";
