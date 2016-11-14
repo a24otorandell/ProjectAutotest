be14b4bda51a1c5e57de63ab017f3a8c6c8917a3
aibanez <sandra23>
2016-11-14 15:24:28 +0100

    Nueva pantalla AT2MDMCL0005 Eliminación de codigo comentado de ComoProcedures.

--- src/main/java/screen/AT2MDMCL0005/AT2MDMCL0005Locators.java
+++ src/main/java/screen/AT2MDMCL0005/AT2MDMCL0005Locators.java
@@ -38,7 +38,18 @@ public class AT2MDMCL0005Locators {
         elements.put("search_i_desc", "//*[contains(@id, 'value80::content')]");
         elements.put("search_i_module", "//*[contains(@id, 'value90::content')]");
         //EDIT
-        elements.put("add_b_save", "//*[contains(@id, 'pcgt1:btn_commitExit')]");
+        elements.put("add_b_save", "//*[contains(@id, 'pcgt1PopUp:boton_commit')]");
         elements.put("add_sl_status", "//*[contains(@id, 'pc1:soc1::content')]");
+        //GET DATA
+        elements.put("table_i_id", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[1]");
+        elements.put("table_i_client_system", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[2]");
+        elements.put("table_i_client_request", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[4]");
+        elements.put("table_i_client_registered", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[3]");
+        elements.put("table_i_date_modified", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[7]");
+        elements.put("table_i_date_received", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[6]");
+        elements.put("table_i_type", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[8]");
+        elements.put("table_i_status", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[10]");
+        elements.put("table_i_desc", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[9]");
+        elements.put("table_i_module", "//*[contains(@id, 'pc1:syncTab::db')]/table/tbody/tr[1]/td[13]");
     }
 }
--- src/main/java/screen/AT2MDMCL0005/AT2MDMCL0005Test.java
+++ src/main/java/screen/AT2MDMCL0005/AT2MDMCL0005Test.java
@@ -6,6 +6,8 @@ import core.CommonActions.Functions;
 import core.TestDriver.TestDriver;
 import core.recursiveData.recursiveXPaths;
 
+import javax.sound.midi.SysexMessage;
+
 /**
  * Created by aibanez on 14/11/2016.
  */
@@ -43,33 +45,122 @@ public class AT2MDMCL0005Test {
     }
 
     protected boolean testCSED(TestDriver driver) {
+        if (!getDatos(driver)) return false;
         if (!search(driver)) return false;
         if (!interaction_edit(driver)) return false;
+        if (!getFechaModified(driver)) return false;
+        if (!search(driver)) return false;
         if (!others_actions(driver)) return false;
         return true;
     }
 
-    private boolean search(TestDriver driver) {
-        driver.getReport().addHeader("SEARCH RECORD", 3, false);
-        String where = " on SEARCH";
-/*        if (!Functions.insertInput(driver, new String[]{"search_i_id", getElements("search_i_id")},
-                "id", "1", where)) {
-            return false;
-        }*/
-        if (!Functions.insertInput(driver, new String[]{"search_i_client_system", getElements("search_i_client_system")},
-                "system","ATLAS", where)) {
-            return false;
-        }
-        if (!Functions.insertInput(driver, new String[]{"search_i_client_registered", getElements("search_i_client_registered")},
-                "regis","MDM", where)) {
-            return false;
-        }
+    public  boolean getDatos (TestDriver driver) {
+        driver.getReport().addHeader("GET DATOS", 3, false);
+        String where = " GET DATOS";
         if (!Functions.insertInput(driver, new String[]{"search_i_date_received_01", getElements("search_i_date_received_01")},
                 "date_1","", where)) {
             return false;
         }
         if (!Functions.insertInput(driver, new String[]{"search_i_date_received_02", getElements("search_i_date_received_02")},
-                "date_2","", where)) {
+                "date_1b","", where)) {
+            return false;
+        }
+        if (!Functions.clickSearchAndResult(driver,
+                new String[]{"search_b_search", getElements("search_b_search")}, //search button
+                new String[]{"synchronization_e_result", getElements("synchronization_e_result")}, //result element
+                where)) {
+            return false;
+        }
+        if(!Functions.getText(driver,new String[]{"table_i_id", getElements("table_i_id")}, // element path
+                "id", // key for data value (the name)
+                where)){return false;}
+        if(!Functions.getText(driver,new String[]{"table_i_client_system", getElements("table_i_client_system")}, // element path
+                "system", // key for data value (the name)
+                where)){return false;}
+        if(!Functions.getText(driver,new String[]{"table_i_client_request", getElements("table_i_client_request")}, // element path
+                "request", // key for data value (the name)
+                where)){return false;}
+        if(!Functions.getText(driver,new String[]{"table_i_client_registered", getElements("table_i_client_registered")}, // element path
+                "regis", // key for data value (the name)
+                where)){return false;}
+        if(!Functions.getText(driver,new String[]{"table_i_date_modified", getElements("table_i_date_modified")}, // element path
+                "date_2", // key for data value (the name)
+                where)){return false;}
+        if(!Functions.getText(driver,new String[]{"table_i_date_received", getElements("table_i_date_received")}, // element path
+                "date_1", // key for data value (the name)
+                where)){return false;}
+        if(!Functions.getText(driver,new String[]{"table_i_type", getElements("table_i_type")}, // element path
+                "type", // key for data value (the name)
+                where)){return false;}
+        Functions.getText(driver, new String[]{"table_i_status", getElements("table_i_status")}, // element path
+                "status", // key for data value (the name)
+                where);
+        if(!Functions.getText(driver,new String[]{"table_i_desc", getElements("table_i_desc")}, // element path
+                "desc", // key for data value (the name)
+                where)){return false;}
+        if(!Functions.getText(driver,new String[]{"table_i_module", getElements("table_i_module")}, // element path
+                "module", // key for data value (the name)
+                where)){return false;}
+        return true;
+    }
+    public boolean getFechaModified (TestDriver driver) {
+        driver.getReport().addHeader("GET FECHA", 3, false);
+        String where = " GET FECHA";
+        if(!Functions.getText(driver,new String[]{"table_i_date_modified", getElements("table_i_date_modified")}, // element path
+                "date_2", // key for data value (the name)
+                where)){return false;}
+        return true;
+    }
+    private boolean search(TestDriver driver) {
+        driver.getReport().addHeader("SEARCH RECORD", 3, false);
+        String where = " on SEARCH";
+        if (!Functions.insertInput(driver, new String[]{"search_i_id", getElements("search_i_id")},
+                "id", getData("id"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_client_system", getElements("search_i_client_system")},
+                "system", getData("system"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_client_registered", getElements("search_i_client_registered")},
+                "regis",getData("regis"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_client_request", getElements("search_i_client_request")},
+                "request",getData("request"), where)) {
+            return false;
+        }
+        if (!Functions.selectText(driver,
+                new String[]{"search_sl_status", getElements("search_sl_status")},
+                getData("status"), "status", where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_date_received_01", getElements("search_i_date_received_01")},
+                "date_1",getData("date_1"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_date_received_02", getElements("search_i_date_received_02")},
+                "date_1b",masDia("date_1"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_date_modified_01", getElements("search_i_date_modified_01")},
+                "date_2",getData("date_2"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_date_modified_02", getElements("search_i_date_modified_02")},
+                "date_2b",masDia("date_2"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_type", getElements("search_i_type")},
+                "type",getData("type"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_desc", getElements("search_i_desc")},
+                "desc",getData("desc"), where)) {
+            return false;
+        }
+        if (!Functions.insertInput(driver, new String[]{"search_i_module", getElements("search_i_module")},
+                "module",getData("module"), where)) {
             return false;
         }
         if (!Functions.clickSearchAndResult(driver,
@@ -80,6 +171,14 @@ public class AT2MDMCL0005Test {
         }
         return true;
     }
+    public String masDia (String data ) {
+        String date = getData(data);
+        String days = date.substring(0,2);
+        int pNumero = Integer.parseInt(days);
+        String fin_date = String.valueOf(pNumero+1);
+        date = date.replace(days, fin_date);
+        return date;
+    }
     private boolean interaction_edit(TestDriver driver) {
         driver.getReport().addHeader("EDIT RECORD", 3, false);
         String where = " on EDITTION";
@@ -89,9 +188,12 @@ public class AT2MDMCL0005Test {
                 where)) {
             return false;
         }
+        Functions.break_time(driver,3 , 600);
         if (!Functions.selectText(driver,
-                new String[]{"add_sl_status",getElements("add_sl_status")},
-                "RESOLVE", "status", where)){return false;}
+                new String[]{"add_sl_status", getElements("add_sl_status")},
+                "IGNORED", "status", where)) {
+                return false;
+        }
         if (!Functions.checkClickByAbsence(driver,
                 new String[]{"add_b_save", getElements("add_b_save")}, //e1
                 recursiveXPaths.glass, //e2
@@ -99,17 +201,8 @@ public class AT2MDMCL0005Test {
         return true;
     }
     private boolean others_actions(TestDriver driver) {
-        driver.getReport().addHeader("OTHER ACTIONS AUDIT DATA", 3, false);
-        String where = " on OTHER AUDIT DATA";
-/*        if (!Functions.auditData(driver,
-                new String[]{"group_b_actions", getElements("group_b_actions")}, //actions button
-                new String[]{"group_b_actions_audit_data", getElements("group_b_actions_audit_data")}, //audit button
-                new String[]{"audit_b_ok", recursiveXPaths.audit_b_ok}, //audit_b_ok
-                where)) {
-            return false;
-        }*/
         driver.getReport().addHeader("OTHER DETACH", 3, false);
-        where = " on OTHER DETACH";
+        String where = " on OTHER DETACH";
         if (!Functions.detachTable(driver,
                 new String[]{"synchronization_b_detach", getElements("synchronization_b_detach")}, //detach button
                 true,     //screenshot??
