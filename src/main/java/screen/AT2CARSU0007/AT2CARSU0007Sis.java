4003d7b996b3fa324f03ec3d6115f1abcfb65683
aibanez <sandra23>
2016-11-22 16:06:29 +0100

    Nueva pantalla AT2CARSU0007 Eliminación de codigo comentado de ComoProcedures.

---
+++ src/main/java/screen/AT2CARSU0007/AT2CARSU0007Sis.java
@@ -0,0 +1,48 @@
+package screen.AT2CARSU0007;
+
+import core.CommonActions.CommonProcedures;
+import core.TestDriver.TestDriver;
+
+/**
+ * Created by aibanez on 22/11/2016.
+ */
+public class AT2CARSU0007Sis {
+    protected AT2CARSU0007Locators locators;
+    protected AT2CARSU0007Data data;
+
+    public AT2CARSU0007Sis() {
+
+    }
+    public AT2CARSU0007Locators getLocators() {
+        return locators;
+    }
+    public void setLocators(AT2CARSU0007Locators locators) {
+        this.locators = locators;
+    }
+    public AT2CARSU0007Data getData() {
+        return data;
+    }
+    public void setData(AT2CARSU0007Data data) {
+        this.data = data;
+    }
+    public void start(TestDriver driver) {
+        setScreenInfo(driver);
+        CommonProcedures.goToScreen(driver);
+    }
+    protected void setScreenInfo(TestDriver driver) {
+        driver.getTestdetails().setMainmenu("Car Hire");
+        driver.getTestdetails().setSubmenu("Setup");
+        driver.getTestdetails().setScreen("Extras Carhire Master Maintenance");
+    }
+    protected String getElements(String key) {
+        return String.valueOf(this.locators.getElements().get(key));
+    }
+    protected String getData(String key) {
+        return String.valueOf(this.data.getData().get(key));
+    }
+
+    protected boolean testCSED(TestDriver driver) {
+        CommonProcedures.styleGuide(driver);
+        return false;
+    }
+}
